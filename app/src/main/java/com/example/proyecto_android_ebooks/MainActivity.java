891755dead04b402dc749ee1a862934a5a06cbba
Manuel Paredes Cerrato <manuel99muh@gmail.com>
2022-01-28 11:19:34 +0100

    pruebas

--- app/src/main/java/com/example/proyecto_android_ebooks/MainActivity.java
+++ app/src/main/java/com/example/proyecto_android_ebooks/MainActivity.java
@@ -11,9 +11,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //asd
-    //as
-    public void prueba(){
-
-    }
 }
